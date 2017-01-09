50a89a3c3617d187fd061fb9f235f0f0dcb793d1
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-10 00:04:56 +0100

    added sources.list and apt-key

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,3 +1,15 @@
 #curl -fsSL https://test.docker.com/ | sed -e 's/\(apt-get install.*docker-engine\)/RUNLEVEL=1 \1/' | /bin/bash
 
-apt-get install -y docker-engine=1.11.2~rc1-0~trusty
+cat <<EOF > /etc/apt/sources.list.d/docker.list
+deb https://apt.dockerproject.org/repo ubuntu-trusty main
+EOF
+
+apt-key adv \
+  --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+apt-get update
+
+apt-cache showpkg docker-engine
+
+apt-get install -y docker-engine=1.11.2-0~trusty
