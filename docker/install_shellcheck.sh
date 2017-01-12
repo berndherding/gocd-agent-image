0c35de80dadd8798ccd3fd04c7ab32b09940193d
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-12 12:01:56 +0100

    make shellcheck work

--- docker/install_shellcheck.sh
+++ docker/install_shellcheck.sh
@@ -1,7 +1,4 @@
-#cat <<EOF >> /etc/apt/sources.list
-#deb http://de.archive.ubuntu.com/ubuntu trusty-backports universe
-#EOF
-#apt-get update
-#apt-get install -y shellcheck
-
+apt-get update
+apt-get install libgmp-dev
 curl -o /usr/bin/shellcheck https://s3.eu-central-1.amazonaws.com/bherding-gocd-agent-image/shellcheck-0.4.4
+chmod 755 /usr/bin/shellcheck
