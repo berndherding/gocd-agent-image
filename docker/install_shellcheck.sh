fe8f45613b4e083d1e6561ea962f39d3fd5931e5
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-12 20:50:11 +0100

    install -y

--- docker/install_shellcheck.sh
+++ docker/install_shellcheck.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install libgmp-dev
+apt-get install -y libgmp-dev
 curl -o /usr/bin/shellcheck https://s3.eu-central-1.amazonaws.com/bherding-gocd-agent-image/shellcheck-0.4.4
 chmod 755 /usr/bin/shellcheck
