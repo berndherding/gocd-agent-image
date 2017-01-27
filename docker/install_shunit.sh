6fe16df51e72511fcb9c6d5ef5f660ee64976d7c
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-27 22:14:51 +0100

    deleted

--- docker/install_shunit.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/kward/shunit2.git
-cp shunit2/source/2.1/src/shunit2 /usr/local/bin/shunit
--- docker/make_port_configurable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i -e 's/:8154//g' /etc/service/go-agent/run
